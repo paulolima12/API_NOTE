9d2001c1946513a268225ab656820aabb1a25319
paulolima12 <paulolima12@gmail.com>
2025-08-05 11:08:18 -0300

    api node

---
+++ api/server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+let users = []
+
+app.get('/cadastro',(req,res)=>{
+    res.json(users)
+})
+
+app.post("/cadastro", (req,res) => {
+    users.push(req.body)
+    res.send("Usuário criado!")
+})
+
+app.listen(3000,()=>{
+    console.log('Servidor....')
+
+})
+
+app.listen(3000)
